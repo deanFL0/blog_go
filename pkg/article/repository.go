d513da8ccba7b52a655d2123e6d256eb3aaa230a
dean <dean.fl20@student.unibi.ac.id>
2025-03-19 12:34:53 +0700

    add article repository

---
+++ pkg/article/repository.go
@@ -0,0 +1,53 @@
+package article
+
+import (
+	"github.com/deanFL0/blog_api_go/pkg/entities"
+	"gorm.io/gorm"
+)
+
+type Repository interface {
+	CreteArticle(article *entities.Article) (*entities.Article, error)
+	ReadArticle(ID int) (*entities.Article, error)
+	ReadArticles() (*[]entities.Article, error)
+	UpdateArticle(article *entities.Article) (*entities.Article, error)
+	DeleteArticle(ID int) error
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepo(db *gorm.DB) Repository {
+	return &repository{
+		db: db,
+	}
+}
+
+func (r *repository) CreteArticle(article *entities.Article) (*entities.Article, error) {
+	return article, r.db.Create(article).Error
+}
+
+func (r *repository) ReadArticle(ID int) (*entities.Article, error) {
+	var article entities.Article
+	result := r.db.Find(&article, ID)
+	return &article, result.Error
+}
+
+func (r *repository) ReadArticles() (*[]entities.Article, error) {
+	var article []entities.Article
+	result := r.db.Find(&article)
+	return &article, result.Error
+}
+
+func (r *repository) UpdateArticle(article *entities.Article) (*entities.Article, error) {
+	return article, r.db.Save(article).Error
+}
+
+func (r *repository) DeleteArticle(ID int) error {
+	var article entities.Article
+	err := r.db.Find(&article, ID).Error
+	if err != nil {
+		return err
+	}
+	return r.db.Delete(&article).Error
+}
