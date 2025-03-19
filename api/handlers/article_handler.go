72f12988f8ad27f79f2abc0899f80ba1cfcf1598
dean <dean.fl20@student.unibi.ac.id>
2025-03-19 13:17:02 +0700

    add article presenter and handler

---
+++ api/handlers/article_handler.go
@@ -0,0 +1,97 @@
+package handlers
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/deanFL0/blog_api_go/api/presenter"
+	"github.com/deanFL0/blog_api_go/pkg/article"
+	"github.com/deanFL0/blog_api_go/pkg/entities"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AddArticle(service article.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var requestBody entities.Article
+		err := c.BodyParser(&requestBody)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		if requestBody.Title == "" || requestBody.Body == "" {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(errors.New("please specify title and body")))
+		}
+		result, err := service.InsertArticle(&requestBody)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		return c.JSON(presenter.ArticleSuccessResponse(result))
+	}
+}
+
+func UpdateArticle(service article.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		var requestBody entities.Article
+		err := c.BodyParser(&requestBody)
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		result, err := service.UpdateArticle(&requestBody)
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		return c.JSON(presenter.ArticleSuccessResponse(result))
+	}
+}
+
+func RemoveArticle(service article.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		err = service.RemoveArticle(int(ID))
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		return c.JSON(&fiber.Map{
+			"status": true,
+			"data":   "Deleted successfully",
+			"err":    nil,
+		})
+	}
+}
+
+func GetArticle(service article.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		ID, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			c.Status(http.StatusBadRequest)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		fetched, err := service.FetchArticle(int(ID))
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		return c.JSON(presenter.ArticleSuccessResponse(fetched))
+	}
+}
+
+func GetArticles(service article.Service) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		fetched, err := service.FetchArticles()
+		if err != nil {
+			c.Status(http.StatusInternalServerError)
+			return c.JSON(presenter.ArticleErrorResponse(err))
+		}
+		return c.JSON(presenter.ArticlesSuccessResponse(fetched))
+	}
+}
---
+++ api/presenter/article.go
@@ -0,0 +1,49 @@
+package presenter
+
+import (
+	"github.com/deanFL0/blog_api_go/pkg/entities"
+	"github.com/gofiber/fiber/v2"
+)
+
+type Article struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+func ArticleSuccessResponse(data *entities.Article) *fiber.Map {
+	article := Article{
+		ID:    data.ID,
+		Title: data.Title,
+		Body:  data.Body,
+	}
+	return &fiber.Map{
+		"status": true,
+		"data":   article,
+		"error":  nil,
+	}
+}
+
+func ArticlesSuccessResponse(data *[]entities.Article) *fiber.Map {
+	var articles []Article
+	for _, article := range *data {
+		articles = append(articles, Article{
+			ID:    article.ID,
+			Title: article.Title,
+			Body:  article.Body,
+		})
+	}
+	return &fiber.Map{
+		"status": true,
+		"data":   articles,
+		"error":  nil,
+	}
+}
+
+func ArticleErrorResponse(err error) *fiber.Map {
+	return &fiber.Map{
+		"status": true,
+		"data":   "",
+		"error":  err.Error(),
+	}
+}
